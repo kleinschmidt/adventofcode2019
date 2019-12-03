a2f6c54581f822e22936eceb7e625272be3f8965
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2019-12-02 19:07:58 -0500

    day 01, star 2

--- 01.jl
+++ 01.jl
@@ -9,3 +9,16 @@ f = x -> x ÷ 3 - 2
 
 map(f, test_input)
 mapreduce(f, +, input, init=0)
+
+function g(x)
+    total = 0
+    fuel = f(x)
+    while fuel > 0
+        total += fuel
+        x, fuel = fuel, f(fuel)
+    end
+    return total
+end
+
+g.(test_input)
+mapreduce(g, +, input, init=0)
