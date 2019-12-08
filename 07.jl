2246a0a71fd99be57f5aed218952900029a4de24
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2019-12-08 15:27:15 -0500

    construct Tasks for communicating between computers

--- 07.jl
+++ 07.jl
@@ -36,19 +36,20 @@ end
 @test star1_serial("3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0", [1,0,4,3,2]) == 65210
 
 function star1_channel(instructions, phases)
-    input = Channel{Int}(Inf)
-    output = input
-    @sync begin 
-        for p in phases
-            c = Computer(instructions, input=output)
-            put!(output, p)
-            output = c.output
-            @async run!(c)
+    computers = [Computer(instructions, input=[p]) for p in phases]
+    for ci in 2:length(computers)
+        println("connecting $(ci-1)→$(ci)")
+        @async begin
+            x = take!(computers[ci-1].output)
+            println("$(ci-1)→$(ci): $x")
+            put!(computers[ci].input, x)
         end
-        put!(input, 0)
     end
-    return take!(output)
+    put!(computers[1].input, 0)
+    @async foreach(run!, computers)
+    return take!(computers[end].output)
 end
+
 
 @test star1_channel("3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0", [4,3,2,1,0]) == 43210
 
