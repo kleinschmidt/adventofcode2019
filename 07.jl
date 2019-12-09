7effdde8224147def75a9cd202894adf5bfdad7e
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2019-12-08 19:38:52 -0500

    refactor tests etc. into functions (use with revise)

--- 07.jl
+++ 07.jl
@@ -1,4 +1,5 @@
 using Test
+using Combinatorics
 
 includet("./intcode.jl")
 using .Intcodes
@@ -26,15 +27,6 @@ function star1_serial(instructions, phases)
 end
 
 
-
-
-@test star1_serial("3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0", [4,3,2,1,0]) == 43210
-
-@test star1_serial("3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0",
-                   0:4) == 54321
-
-@test star1_serial("3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0", [1,0,4,3,2]) == 65210
-
 function star1_channel(instructions, phases)
     computers = [Computer(instructions, input=[p]) for p in phases]
     for ci in 2:length(computers)
@@ -51,17 +43,24 @@ function star1_channel(instructions, phases)
 end
 
 
-@test star1_channel("3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0", [4,3,2,1,0]) == 43210
+function star1_tests()
 
-@test star1_channel("3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0", 0:4) == 54321
+    @test star1_serial("3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0", [4,3,2,1,0]) == 43210
 
-@test star1_channel("3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0", [1,0,4,3,2]) == 65210
+    @test star1_serial("3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0",
+                       0:4) == 54321
 
-input = read("07.input", String)
+    @test star1_serial("3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0", [1,0,4,3,2]) == 65210
 
-using Combinatorics
+    @test star1_channel("3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0", [4,3,2,1,0]) == 43210
 
-input_ints = parse.(Int, split(chomp(input), ','))
+    @test star1_channel("3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0", 0:4) == 54321
 
-maximum(star1_channel(input_ints, phases) for phases in permutations(0:4))
+    @test star1_channel("3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0", [1,0,4,3,2]) == 65210
+end
 
+function star1()
+    input = read("07.input", String)
+    input_ints = parse.(Int, split(chomp(input), ','))
+    maximum(star1_channel(input_ints, phases) for phases in permutations(0:4))
+end
