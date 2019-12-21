be2f4d07d71628e51d1f280ec9303d9c738a935c
Dave Kleinschmidt <dave.f.kleinschmidt@gmail.com>
2019-12-20 21:26:01 -0500

    19.2, slow and dumb

--- 19.jl
+++ 19.jl
@@ -1,16 +1,21 @@
 includet("./intcode.jl")
 using .Intcodes
 
+instr = parse.(Int, split(chomp(read("19.input", String)), ','))
+
+function drone(x,y)
+    c = Computer(instr)
+    @async run!(c)
+    put!(c.input, x)
+    put!(c.input, y)
+    beam = take!(c.output)
+end
 
 function star1()
-    instr = parse.(Int, split(chomp(read("19.input", String)), ','))
     beamed = 0
-    for y in 1:50
-        for x in 1:50
-            c = Computer(instr)
-            @async run!(c)
-            put!.(Ref(c.input), (x,y).-1)
-            beam = take!(c.output)
+    for y in 0:49
+        for x in 0:49
+            beam = drone(x,y)
             print(beam == 1 ? '#' : '.')
             beamed += beam
         end
@@ -21,3 +26,26 @@ end
 
 
 
+# need to find the closest 100×100 square enclosed in the beam.  that's the same
+# as finding the first bottom edge (x,y) where (x+99, y-99) is also in the beam
+function star2()
+    # start is a little tricky since the drone doesn't pick up the beam for a
+    # few rows...
+    y = 100
+    x = 0
+    while drone(x,y) == 0
+        x += 1
+    end
+
+    while drone(x+99, y-99) == 0
+        while drone(x,y) == 1
+            y += 1
+        end
+        while drone(x,y) == 0
+            x += 1
+        end
+        # @show x,y
+    end
+
+    return x*10_000 + y - 99
+end
